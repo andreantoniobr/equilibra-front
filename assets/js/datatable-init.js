91b45dfa08d283d592a4b6f4da55dd4f805b3def
André Antônio <andreantoniobr@hotmail.com>
2025-06-05 22:47:46 -0300

    feat: added more pages

--- assets/js/datatable-init.js
+++ assets/js/datatable-init.js
@@ -24,3 +24,28 @@ $(document).ready(function() {
   }
 });
 
+
+$(document).ready(function() {
+  if (!$.fn.dataTable.isDataTable('#reports')) {
+    $('#reports').DataTable({
+      columnDefs: [
+            { width: '10%', targets: 0 },  // Primeira coluna (Data)
+            { width: '30%', targets: 1 },  // Segunda coluna (Descrição)
+            { width: '30%', targets: 2 },  // Terceira coluna (Categoria)
+            { width: '15%', targets: 3 },  // Quarta coluna (Valor)
+            { width: '15%', targets: 4 },  // Quinta coluna (Status)
+        ],
+        pageLength: 10,
+        lengthMenu: [ [5, 10, 25, 50, -1], [5, 10, 25, 50, "Todos"] ], 
+      "language": {
+        "sProcessing": "Processando...",
+        "sLengthMenu": "Exibir _MENU_ registros por página",
+        "sZeroRecords": "Nenhum registro encontrado",
+        "sInfo": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrando 0 a 0 de 0 registros",
+        "sInfoFiltered": "(filtrado de _MAX_ registros no total)",
+        "sSearch": "Pesquisar:"       
+      }
+    });
+  }
+});
