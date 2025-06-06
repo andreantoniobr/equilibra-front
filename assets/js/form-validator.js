fa330582b73eb701acfc4aa3b2f19f795abe2f71
André Antônio <andreantoniobr@hotmail.com>
2025-06-06 13:06:37 -0300

    fix: minor changes in form validators

--- assets/js/form-validator.js
+++ assets/js/form-validator.js
@@ -1,16 +1,22 @@
 document.getElementById("entryForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    let amount = document.getElementById("entryAmount").value;
-    let date = document.getElementById("entryDate").value;    
+    const entryAmount = document.getElementById("entryAmount");
+    const entryDate = document.getElementById("entryDate");
 
-    if (amount === "") {
-        alert("Por favor, preencha o valor.");        
+    let amount = entryAmount.value;
+    let date = entryDate.value;    
+
+    if (amount === "") {   
+        addAlert("Por favor, preencha o valor.", 'danger', '#modal-alerts')     
     } else if (date === "") {
-        alert("Por favor, preencha a data!");        
+        addAlert("Por favor, preencha a data!", 'danger', '#modal-alerts')        
     } else {
-        alert("Formulário enviado com sucesso!");
+        addAlert("Cadastrado com sucesso!", 'success', '#page-alerts')
         hideModal("entryModal");
+        entryAmount.value = '';
+        entryDate.value = '';
+        removeAlertsInSelector('#modal-alerts');
     }
 });
 
@@ -18,4 +24,33 @@ function hideModal(modalID) {
     let modalElement = document.getElementById(modalID);
     let modal = bootstrap.Modal.getInstance(modalElement);
     modal.hide();
+}
+
+function addAlert(message, type = 'primary', selector = 'body') {
+  const alertElement = document.createElement('div');
+  alertElement.className = `alert alert-${type} alert-dismissible fade show`;
+  alertElement.role = 'alert';
+  alertElement.innerHTML = `
+    ${message}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  `;
+
+  if (selector === 'body') {
+    document.body.prepend(alertElement);
+  } else {
+    const element = document.querySelector(selector);
+    if (element) {
+      removeAlertsInElement(element);
+      element.appendChild(alertElement); 
+    }
+  }  
+}
+
+function removeAlertsInSelector(selector) {
+    const element = document.querySelector(selector);
+    removeAlertsInElement(element);
+}
+
+function removeAlertsInElement(element) {
+    element.innerHTML = ''; 
 }
